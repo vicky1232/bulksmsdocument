559c68245f3a487aff42ec66e6f1094de9e4a1d5
shagun <shagun.kaushik@dbalounge.com>
2024-09-09 18:31:56 +0530

    registration Api

--- src/main/java/com/bulkSms/ServiceImpl/ServiceImpl.java
+++ src/main/java/com/bulkSms/ServiceImpl/ServiceImpl.java
@@ -2,8 +2,12 @@ package com.bulkSms.ServiceImpl;
 
 
 import com.bulkSms.Entity.BulkSms;
+import com.bulkSms.Entity.Role;
+import com.bulkSms.Entity.UserDetail;
 import com.bulkSms.Model.CommonResponse;
+import com.bulkSms.Model.RegistrationDetails;
 import com.bulkSms.Repository.BulkRepository;
+import com.bulkSms.Repository.UserDetailRepo;
 import com.bulkSms.Service.Service;
 import com.bulkSms.Utility.CsvFileUtility;
 import com.bulkSms.Utility.EncodingUtils;
@@ -11,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -30,6 +35,10 @@ public class ServiceImpl implements Service {
     private EncodingUtils encodingUtils;
     @Value("${project.save.path}")
     private String projectSavePath;
+    @Autowired
+    private UserDetailRepo userDetailRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public ResponseEntity<CommonResponse> fetchPdf(String folderPath) {
         CommonResponse commonResponse = new CommonResponse();
@@ -81,5 +90,28 @@ public class ServiceImpl implements Service {
             commonResponse.setMsg("File is not a csv file");
         }
         return ResponseEntity.ok(commonResponse);
+    }
+
+    @Override
+    public void registerNewUser(RegistrationDetails registerUserDetails) throws Exception {
+        if(userDetailRepo.findByEmailId(registerUserDetails.getEmailId()).isPresent()){
+            throw new Exception("EmailID already exist");
+        }
+        UserDetail userDetails = new UserDetail();
+        userDetails.setFirstname(registerUserDetails.getFirstName());
+        userDetails.setLastName(registerUserDetails.getLastName());
+        userDetails.setEmailId(registerUserDetails.getEmailId());
+        userDetails.setMobileNo(registerUserDetails.getMobileNo());
+        userDetails.setPassword(passwordEncoder.encode(registerUserDetails.getPassword()));
+
+        Role role = new Role();
+        role.setRole("ROLE_USER");
+        role.setUserMaster(userDetails);
+
+        userDetails.setRoleMaster(role);
+
+        userDetailRepo.save(userDetails);
+
+        registerUserDetails.setRole(role.getRole());
     }
 }
