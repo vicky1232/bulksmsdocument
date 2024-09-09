34791dc752031f22ca8e05cdde55c49eb4401a72
Atishra <atish.rai@dbalounge.com>
2024-09-09 18:00:38 +0530

    csvupload api

--- src/main/java/com/bulkSms/Controller/Admin.java
+++ src/main/java/com/bulkSms/Controller/Admin.java
@@ -2,8 +2,13 @@ package com.bulkSms.Controller;
 
 import com.bulkSms.Service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.bulkSms.Model.CommonResponse;
+import com.bulkSms.Service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/admin")
@@ -15,5 +20,5 @@ public class Admin {
     public ResponseEntity<String> postMessage() {
         return ResponseEntity.ok("Message received by admin successfully!");
     }
-}
 
+}
--- src/main/java/com/bulkSms/Controller/Login.java
+++ src/main/java/com/bulkSms/Controller/Login.java
@@ -1,9 +1,12 @@
 package com.bulkSms.Controller;
 
 import com.bulkSms.JwtAuthentication.JwtHelper;
+import com.bulkSms.Model.CommonResponse;
 import com.bulkSms.Model.JwtRequest;
 import com.bulkSms.Model.JwtResponse;
 import com.bulkSms.Service.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -26,8 +30,11 @@ public class Login {
     private AuthenticationManager manager;
     @Autowired
     private Service service;
+
     @Autowired
     private JwtHelper helper;
+
+    Logger logger = LoggerFactory.getLogger(Login.class);
 
 
     @PostMapping("/login")
@@ -67,5 +74,15 @@ public class Login {
     @GetMapping("/fetch-pdf")
     public ResponseEntity<?> pdfFetcherFromLocation(@RequestParam(name = "pdfUrl") String pdfUrl) throws IOException {
         return service.fetchPdf(pdfUrl);
+    }
+    @PostMapping("/csvUpload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+        CommonResponse commonResponse = new CommonResponse();
+        try {
+            return ResponseEntity.ok(service.save(file).getBody());
+        } catch (Exception e) {
+            commonResponse.setMsg("Technical issue : " + e.getMessage());
+            return new ResponseEntity<>(commonResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
